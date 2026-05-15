bb1fb3f3ec0cb70c9629f8c2045d10ff8046566b
LAGGOUNE Omar <dogie4bogie@gmail.com>
2026-05-15 23:52:00 +0100

    first and maybe last commit, game finished

---
+++ src/main/java/com/learning/Main.java
@@ -0,0 +1,96 @@
+package com.learning;
+
+import java.util.Random;
+import java.util.Scanner;
+import java.awt.*;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args){
+        // Java Dice roller program
+
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int numOfDice;
+        int total=0;
+
+        System.out.println("Enter the # of dice to roll:");
+        numOfDice = scanner.nextInt();
+
+        if (numOfDice > 0) {
+            for (int i = 0; i <numOfDice ; i++) {
+                int roll = random.nextInt(1,7);
+                printDie(roll);
+                System.out.println("You rolled: "+roll);
+                total +=roll;
+            }
+
+            System.out.println("Total: "+total);
+
+        }else {
+            System.out.println("# of dice must be greater than 0");
+        }
+        // Declare variables
+        //Get # of dice from the user
+        //check if # of dice > 0
+        //roll all the dice
+        //get the total
+        //display ascii of dice
+    }
+
+    static void printDie(int roll){
+        String dice1="""
+                   -------
+                  |       |
+                  |   ●   |
+                  |       |
+                   -------
+                  """;
+        String dice2="""
+                   -------
+                  | ●     |
+                  |   ●   |
+                  |       |
+                   -------
+                  """;
+        String dice3="""
+                   -------
+                  | ●     |
+                  |   ●   |
+                  |     ● |
+                   -------
+                  """;
+        String dice4="""
+                   -------
+                  | ●   ● |
+                  |       |
+                  | ●   ● |
+                   -------
+                  """;
+        String dice5="""
+                   -------
+                  | ●   ● |
+                  |   ●   |
+                  | ●   ● |
+                   -------
+                  """;
+        String dice6="""
+                   -------
+                  | ●   ● |
+                  | ●   ● |
+                  | ●   ● |
+                   -------
+                  """;
+        switch (roll){
+            case 1 -> System.out.print(dice1);
+            case 2 -> System.out.print(dice2);
+            case 3 -> System.out.print(dice3);
+            case 4 -> System.out.print(dice4);
+            case 5 -> System.out.print(dice5);
+            case 6 -> System.out.print(dice6);
+            default -> System.out.println("Invalid roll!");
+        }
+
+    }
+}
